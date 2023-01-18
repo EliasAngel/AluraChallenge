87eca4a97ac3db36388d781e4012c3a683cb8b9a
Elías <ecassini25@gmail.com>
2023-01-18 14:40:55 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,40 @@
+/*Claves de encriptación
+e=enter
+i=imes
+a=ai
+o=ober
+u=ufat*/
+
+function btnEncriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm, "enter");
+    var txtCifrado = txtCifrado.replace(/i/igm, "imes");
+    var txtCifrado = txtCifrado.replace(/a/igm, "ai");
+    var txtCifrado = txtCifrado.replace(/o/igm, "ober");
+    var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
+
+    document.getElementById("resultado").style.background ="none";
+    document.getElementById("resultado").innerHTML = txtCifrado;
+}
+
+function btnDesencriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm, "e");
+    var txtCifrado = txtCifrado.replace(/imes/igm, "i");
+    var txtCifrado = txtCifrado.replace(/ai/igm, "a");
+    var txtCifrado = txtCifrado.replace(/ober/igm, "o");
+    var txtCifrado = txtCifrado.replace(/ufat/igm, "u");
+
+    document.getElementById("resultado").style.background ="none";
+    document.getElementById("resultado").innerHTML = txtCifrado;
+}
+
+function btnCopiar() {
+    navigator.permissions.query({name: "clipboard-write"}).then((result) => {
+        if (result.state === "granted" || result.state === "prompt") {
+          /* write to the clipboard now */
+        }
+      });
+    navigator.clipboard.writeText("resultado").then
+    
+}
